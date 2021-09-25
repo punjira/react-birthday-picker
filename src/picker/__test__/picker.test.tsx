08deddec21f09cb3e1b810c5690ddaf38c591218
punjira <saeedwmail@gmail.com>
2021-09-25 16:52:36 +0330

    fix import

--- src/picker/__test__/picker.test.tsx
+++ src/picker/__test__/picker.test.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import Picker from "../..";
+import { BirthdayPicker } from "../..";
 
 describe("BIRTHDAY PICKER COMPONENT", () => {
   it("should render", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     expect(screen.getByPlaceholderText("day")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("month")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("year")).toBeInTheDocument();
   });
   it("should change place holder with input props", () => {
-    render(<Picker placeHolders={["one", "two", "three"]} />);
+    render(<BirthdayPicker placeHolders={["one", "two", "three"]} />);
     expect(screen.getByPlaceholderText("one")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("two")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("three")).toBeInTheDocument();
@@ -20,42 +20,42 @@ describe("BIRTHDAY PICKER COMPONENT", () => {
     expect(screen.queryByPlaceholderText("year")).not.toBeInTheDocument();
   });
   it("should change focus from day to month if value is grater than 3", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     fireEvent.change(screen.getByPlaceholderText("day"), {
       target: { value: 5 }
     });
     expect(screen.getByPlaceholderText("month")).toHaveFocus();
   });
   it("should change focus from day to month if value length is 2", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     fireEvent.change(screen.getByPlaceholderText("day"), {
       target: { value: "03" }
     });
     expect(screen.getByPlaceholderText("month")).toHaveFocus();
   });
   it("should change focus from month to year if value is grater than 2", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     fireEvent.change(screen.getByPlaceholderText("month"), {
       target: { value: 2 }
     });
     expect(screen.getByPlaceholderText("year")).toHaveFocus();
   });
   it("should change focus from month to year if value length is 2", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     fireEvent.change(screen.getByPlaceholderText("month"), {
       target: { value: "12" }
     });
     expect(screen.getByPlaceholderText("year")).toHaveFocus();
   });
   it("should change focus from month to year if value length is 2", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     fireEvent.change(screen.getByPlaceholderText("month"), {
       target: { value: "01" }
     });
     expect(screen.getByPlaceholderText("year")).toHaveFocus();
   });
   it("should change focus from year to month if form is receiving backspace and is already empty", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     const yearInput = screen.getByPlaceholderText("year");
     yearInput.focus();
     fireEvent.keyDown(yearInput, {
@@ -64,7 +64,7 @@ describe("BIRTHDAY PICKER COMPONENT", () => {
     expect(screen.getByPlaceholderText("month")).toHaveFocus();
   });
   it("should change focus from month to day if form is receiving backspace and is already empty", () => {
-    render(<Picker />);
+    render(<BirthdayPicker />);
     const monthInput = screen.getByPlaceholderText("month");
     monthInput.focus();
     fireEvent.keyDown(monthInput, {
