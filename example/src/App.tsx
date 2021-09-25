af46313190cfba2f37afd38700f7d0490eb517ec
Saeed Ghotb <saeedghotb95@gmail.com>
2021-09-25 13:34:49 +0330

    add demo project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,22 @@
-import { FC } from "react";
+import moment from "moment";
+import React, { useState } from "react";
+import Picker from "react-birthday-picker";
 
-const App: FC = () => {
-  return <div></div>;
-};
+function App() {
+  const [date, setDate] = useState("");
+  const handleChange = (date: string | null) => {
+    if (date) setDate(date);
+  };
+  return (
+    <div className="App">
+      <Picker
+        onChange={handleChange}
+        placeHolders={["روز", "ماه", "سال"]}
+        style={{ width: "200px" }}
+      />
+      <h2>{moment(date, "YYYY/MM/DD").unix()}</h2>
+    </div>
+  );
+}
 
 export default App;
