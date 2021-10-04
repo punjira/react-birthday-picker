e1a19a55fecf3fd13a3b656e9a45b038d8df21b9
punjira <saeedwmail@gmail.com>
2021-10-04 22:08:25 +0330

    update pre commit to avoid commits on master and development branches

--- scripts/pre-commit.ts
+++ scripts/pre-commit.ts
@@ -2,30 +2,25 @@
  * avoid commits to long living branches
  */
 
-const { exec } = require("child_process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const RED = "\x1b[31m";
 const RESET = "\x1b[0m";
 const YELLOW = "\x1b[33m";
 
-(function () {
-  exec("git rev-parse --abbrev-ref HEAD", function (error, stdout, stderr) {
-    if (
-      stdout === "dev" ||
-      stdout === "develop" ||
-      stdout === "development" ||
-      stdout === "master"
-    ) {
-      console.log(
-        RED +
-          " You can't commit on " +
-          YELLOW +
-          " " +
-          stdout +
-          " branch" +
-          RESET
-      );
-      process.exit(1);
-    }
-  });
+(async function () {
+  const { stdout, stderr } = await exec("git rev-parse --abbrev-ref HEAD");
+  const branch = stdout.replace(/\s/g, "");
+  if (
+    branch === "dev" ||
+    branch === "develop" ||
+    branch === "development" ||
+    branch === "master"
+  ) {
+    console.log(
+      RED + " You can't commit on " + YELLOW + " " + branch + " branch" + RESET
+    );
+    process.exit(1);
+  }
 })();
